cc8323405c461d54070bcaf447883e6375662e67
heraden <127416579+heraden@users.noreply.github.com>
2023-04-21 19:34:29 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-function product() {  
-    const productName = prompt("Enter product name");  
-    const unitePrice =  prompt("Enter unite price");  
-    const quantity =  prompt("Enter quantity"); 
-    const participates=  prompt("Participates in the promotion or not ");  
-    const discount  = unitePrice * 0.1 
-    if (  quantity>= 5) { 
-       console.log(discount); 
-    }  
-     const discount2 = unitePrice * 0.15
-    if ( participates == "yes") { 
-      console.log(discount2); 
-       
-    }else if ( participates == "no"){ 
-      console.log(unitePrice) 
-    } 
-  }  
-   
-  const customerInfo = product();  
-  console.log(customerInfo);
-
-
-  
+document.getElementById("bottom").addEventListener("click", function () { 
+    event.preventDefault(); 
+     let productName = document.getElementById("product").value 
+     let unitePrice= document.getElementById("prize").value 
+     let quantity =document.getElementById("quantity").value 
+     let participates =document.getElementById("promotion").value 
+     const amount = unitePrice * 0.75 
+     if (quantity >= 5 && participates == "yes") { 
+        return console.log(amount) 
+     } 
+     const discount = unitePrice * 0.9  
+     if (quantity >= 5) {  
+        return console.log(discount);  
+         
+     }   
+     const discount2 = unitePrice * 0.85  
+     if (participates == "yes") {  
+        return  console.log(discount2);  
+              
+     } else if (participates == "no"){  
+        return console.log(unitePrice); 
+         
+     }  
+    })
