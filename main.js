40275ff4487731439a17bf29ae2a298a48f65fef
heraden <127416579+heraden@users.noreply.github.com>
2023-04-22 01:09:49 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-document.getElementById("bottom").addEventListener("click", function () { 
+document.getElementId("bottom").addListener("click", function () { 
     event.preventDefault(); 
-     let productName = document.getElementById("product").value 
-     let unitePrice= document.getElementById("prize").value 
-     let quantity =document.getElementById("quantity").value 
-     let participates =document.getElementById("promotion").value 
+     let productName = document.getElementId("product").value 
+     let unitePrice= document.getElementId("prize").value 
+     let quantity =document.getElementId("quantity").value 
+     let participates =document.getElementId("promotion").value 
      const amount = unitePrice * 0.75 
      if (quantity >= 5 && participates == "yes") { 
         return console.log(amount) 
      } 
      const discount = unitePrice * 0.9  
      if (quantity >= 5) {  
-        return console.log(discount);  
+        return console.log(discount1);  
          
      }   
      const discount2 = unitePrice * 0.85  
